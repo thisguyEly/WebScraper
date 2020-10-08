638b3974621e1e094502d2a4437fc78410ad3358
Ely Sanneman <65604667+thisguyEly@users.noreply.github.com>
2020-10-08 19:06:56 -0400

    Add files via upload

--- webScraperAtom.py
+++ webScraperAtom.py
@@ -1,9 +1,11 @@
+# Pandas for converting resultset to csv
 import pandas as pd
 import scipy as sy
 import pytesseract as pt
 import scrapy.crawler
 import scrapy
 
+# BeautifulSoup for scraping html in sites
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen
 
