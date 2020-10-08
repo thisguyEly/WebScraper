6f7bff2ac6160f3624a17777b68121d7b71fe232
Ely Sanneman <65604667+thisguyEly@users.noreply.github.com>
2020-10-08 19:03:19 -0400

    Add files via upload

--- webScraperAtom.py
+++ webScraperAtom.py
@@ -1,3 +1,18 @@
-import pandas 
-import scipy
-import pytesseract
+import pandas as pd
+import scipy as sy
+import pytesseract as pt
+import scrapy.crawler
+import scrapy
+
+from bs4 import BeautifulSoup as bs
+from urllib.request import urlopen
+
+# 1st reference site = featoflouisville
+url = "https://featoflouisville.org/"
+page = urlopen(url)
+html_bytes = page.read()
+html = html_bytes.decode("utf-8")
+
+# start scrapes
+def scrape():
+    for i in links:
